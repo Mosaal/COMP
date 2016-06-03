ec3cfe5134610a2e4a2b828ef0ecf8331cab45f0
Pedro Romano Barbosa <pdrobarbosa@gmail.com>
2016-06-03 02:47:42 +0100

    Added more fields in CFGNode

--- Project/src/CFGNode.java
+++ Project/src/CFGNode.java
@@ -12,12 +12,30 @@ public class CFGNode {
 	
 	/* Assignement or declaration */
 	public boolean twoSides;
+	
 	public String lhsId;
 	public String lhsAccess;
 	public String lhsType;
 	public String lhsArrayIndexId;
 	public String lhsArrayAccess;
+	
+	public String rhs1Id;
+	public String rhs1Access;
+	public String rhs1Type;
+	public String rhs1ArrayIndexId;
+	public String rhs1ArrayAccess;
+	
+	public String rhs2Id;
+	public String rhs2Access;
+	public String rhs2Type;
+	public String rhs2ArrayIndexId;
+	public String rhs2ArrayAccess;
+	
 	public boolean newVar;
+	
+	/* Conditions */
+	
+	/* Call */
 	
 	/* For printing purposes */
 	public boolean visited;
